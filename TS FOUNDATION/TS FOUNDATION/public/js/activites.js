b78b7e2b3a403c33bfb96320928a8567f1afa490
Ankit Negi <89020930+serAnkii@users.noreply.github.com>
2022-09-30 08:27:37 +0530

    Delete TS FOUNDATION/TS FOUNDATION directory

--- TS FOUNDATION/TS FOUNDATION/public/js/activites.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#activity_img").on("change", function(e) {
-    let filename = e.target.files[0].name;
-    $(this)
-      .next(".custom-file-label")
-      .html(filename);
-  });
-});
